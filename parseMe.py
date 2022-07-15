91ab3d6fa3dd0216bd64e9d87bbb53f34c6cd343
mohomar84 <49999850+mohomar84@users.noreply.github.com>
2022-07-15 16:19:45 +0100

    Update parseMe.py

--- parseMe.py
+++ parseMe.py
@@ -5,12 +5,22 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('payload.xml')
 root = tree.getroot()
-
+iter = tree.iter()
 # all items data
 print('Expertise Data:')
 
-for elem in root:
+
+for elem in iter:
+   print('level 1')
+   print(elem)
    for subelem in elem:
+      print('level 2')
+      subelem
       print(subelem.text)
-      for subelem in elem:
-        print(subelem.text)
+      for subelem in subelem:
+         print('level 3')
+         print(subelem.text)
+
+
+
+# check if the element has sub element using -- print(len(tree.findall(".")))
